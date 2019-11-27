d117d2e0155ef86fbe6507bbf9317ce973222277
cogito288 <cogito288@gmail.com>
2019-11-27 23:22:45 +0900

    correct matlab util

--- python-refactor/Test/pre_01_GOCI_test.py
+++ python-refactor/Test/pre_01_GOCI_test.py
@@ -19,15 +19,19 @@ import subprocess
 #import arcpy
 #from arcpy import env
 
-flist = ['/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_01_02.py',
+flist = [
+'/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_01_extract_variables.py',    
+'/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_02_generate_missing_nan_file.py',    
+#'/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_01_02.py',
 '/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_03_AOD_filter.py',
 '/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_04_filtering.py',
 '/home/sehyun/GEMS_python/python-refactor/Code/pre_01_raw/GOCI_05_BL_1km.py',]
 
 for filename in flist:
+    print (f'############################ {filename} ############################')
     cmd = ['/home/sehyun/GEMS_python/venv/bin/python', filename]
     #subprocess.call(cmd)
-
+    tStart = time.time()
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     # Grab stdout line by line as it becomes available.  This will loop until 
     # p terminates.
@@ -37,7 +41,8 @@ for filename in flist:
     # When the subprocess terminates there might be unconsumed output 
     # that still needs to be processed.
     print (p.stdout.read())
-    break
+    tElapsed = time.time() - tStart
+    print (f'Time taken: {tElapsed}')
 
 
 
