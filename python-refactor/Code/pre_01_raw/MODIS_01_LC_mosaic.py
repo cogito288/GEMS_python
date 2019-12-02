428b2892549bdf1258198deeea6dc182b3aeb272
seoh222 <57383359+seoh222@users.noreply.github.com>
2019-12-02 21:47:28 +0900

    Update MODIS_01_LC_mosaic.py

--- python-refactor/Code/pre_01_raw/MODIS_01_LC_mosaic.py
+++ python-refactor/Code/pre_01_raw/MODIS_01_LC_mosaic.py
@@ -6,24 +6,16 @@ project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
 from Code.utils import matlab
 
-import scipy.io as sio
-import numpy as np
 import glob
-import time
-import h5py 
-import pygrib
-import re
+import time 
 from osgeo import gdal
 import tempfile
-#import arcpy
-#from arcpy import env
-
+import subprocess
 
 ### Setting path
-data_base_dir = os.path.join('/', 'media', 'sf_GEMS', 'Data')
-raw_modis_path = os.path.join(data_base_dir, 'Raw', 'MODIS', 'MCD12Q1') 
-path_mosaic = os.path.join(data_base_dir, 'Preprocessed_raw', 'MODIS', '01mosaic') #workspace = os.path.join(work_path, '01_mosaic')
-tmpdirname = tempfile.TemporaryDirectory(dir=base_dir)  # should call clean up to delete
+data_base_dir = os.path.join('/', 'share','irisnas6', 'Data','MODIS_tile')
+raw_modis_path = os.path.join(data_base_dir, '00raw', 'MCD12Q1') 
+path_mosaic = os.path.join('/','share','irisnas5','GEMS','GEMS_python', 'Preprocessed_raw', 'MODIS', '01mosaic') #workspace = os.path.join(work_path, '01_mosaic')
 #path_data="\\\\10.72.26.46\\irisnas6\\Data\\MODIS_tile\\"
 #path_work=path_data+"02region\\EastAsia\\MCD12Q1\\"
 #path_raw = path_data+"00raw\\MCD12Q1\\"
@@ -31,33 +23,28 @@ tmpdirname = tempfile.TemporaryDirectory(dir=base_dir)  # should call clean up t
 flist = glob.glob(os.path.join(raw_modis_path, "*.hdf"))
 flist = [os.path.basename(f) for f in flist]
 flist.sort()
-nfile = len(flist)
 
-for k in range(0,nfile,14):
+for k in range(0,len(flist),14):
+    tmpdirname = tempfile.TemporaryDirectory(dir=base_dir)  # should call clean up to delete
     flist_temp = flist[k:k+14]
     yr = flist_temp[0][9:13]
-    #print (flist_temp[0])
-    #print (yr)
-
     for m in range(0,14):
-        # Extract Subdataset
         fname = flist_temp[m]
         dst_dataset = os.path.join(tmpdirname.name, f"LC_{yr}_{m+1}.tif")
-        #print (fname, dst_dataset)
         
         gdal_dataset = gdal.Open(os.path.join(raw_modis_path, fname))
         src_dataset = gdal_dataset.GetSubDatasets()[0][0]
-        subprocess.call(["gdal_translate", dset, dst_dataset])
+    
+        subprocess.call(["gdal_translate", src_dataset, dst_dataset])
         
         if m==0:
             input_files = [dst_dataset]
         else:
             input_files.append(dst_dataset)
-    
+            
     # Mosaic
     matlab.check_make_dir(path_mosaic) # debugging
-    fname = f"EA_MCD12Q1_mosaic_{yr}.tif"
-    out_filename = os.path.join(path_mosaic, fname)
+    out_filename = os.path.join(path_mosaic, f"EA_MCD12Q1_mosaic_{yr}.tif")
     pixel_type = 'Int16'
     cmd = ["gdal_merge.py", "-init", "255", "-ot", pixel_type, "-o", out_filename]
     cmd = cmd + input_files
