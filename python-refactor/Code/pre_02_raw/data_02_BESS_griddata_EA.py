2bceea2cbfc8d45801bf4f16ffeea86eec74b1b2
cogito288 <cogito288@gmail.com>
2019-12-10 22:49:56 +0900

    working on pre 02 data 02

--- python-refactor/Code/pre_02_raw/data_02_BESS_griddata_EA.py
+++ python-refactor/Code/pre_02_raw/data_02_BESS_griddata_EA.py
@@ -1,46 +1,52 @@
-import os
+### Package Import
 import sys
-project_path = '/home/cogito/Desktop/GEMS_python/matlab2python/python-refactor'
+import os
+base_dir = os.environ['GEMS_HOME']
+project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
+from Code.utils import matlab
 
-from Code.utils.matlab import *
-
+import netCDF4
 import numpy as np
-#from matplotlib.pyplot import imread
-from scipy.interploate import gridata
+import glob
+from scipy.interpolate import griddata
 
-#% path_data = '//10.72.26.46/irisnas6/Data/Aerosol/';
-#% path = '//10.72.26.46/irisnas5/Work/Aerosol/';
+### Setting path
+data_base_dir = os.path.join('/', 'media', 'sf_GEMS', 'Data')
+raw_data_path = os.path.join(data_base_dir, 'Raw', 'BESS') 
+write_path = os.path.join(data_base_dir, 'Preprocessed_raw', 'BESS')
 
-path_data = os.path.join('/', 'share', 'irisnas6', 'Data', 'Aerosol')
-#% path = '/share/irisnas6/Data/Aerosol_Work/EA_GOCI6km/';
-path_bess = os.path.join('/', 'share', 'irisnas6', 'Data', 'BESS', '00raw', 'BESS_RSDN_Daily')
-
-
-mat = matlab.loadmat(os.path.join(path_data, 'grid', 'grid_bess.mat')) # lon_bess, lat_bess
+mat = matlab.loadmat(os.path.join(data_base_dir, 'grid', 'grid_bess.mat')) # lon_bess, lat_bess
 lon_bess = mat['lon_bess']
 lat_bess = mat['lat_bess'] 
+del mat
 
-mat = matlab.loadmat(os.path.join(path_data, 'grid', 'grid_goci.mat')) # lon_goci, lat_goci
+mat = matlab.loadmat(os.path.join(data_base_dir, 'grid', 'grid_goci.mat')) # lon_goci, lat_goci
 lon_goci = mat['lon_goci']
 lat_goci = mat['lat_goci']
+del mat
 
 #%% Until 2016 (nc)
-"""
-    % for yr=2016 %:2016
-    %     cd([path_data2, '00_raw_data/BESS/BESS_RSDN_Daily/',num2str(yr)])
-    %     list = dir('*.nc');
-    %     list = {list.name}';
-    %
-    %     for i=20:22 % 1:length(list)
-    %         bess = ncread(list{i}, 'surface_downwelling_shortwave_flux_in_air'); % 자료 읽기
-    %         bess(bess==-9999)=NaN;
-    %         RSDN = griddata(lon_bess,lat_bess,bess,lon_goci,lat_goci,'linear'); %리샘플링 (0.05 degree를 6km goci 격자로)
-    %         save([path,'BESS/',num2str(yr),'/EA6km_BESS_RSDN_',num2str(yr),'_',list{i}(end-5:end-3),'.mat'],'RSDN')
-    %         disp([list{i},'... i=',num2str(i),' \n'])
-    %     end
-    %     disp(yr)
-    % end
+YEARS = [2016]
+for yr in YEARS:
+    file_list = glob.glob(os.path.join(raw_data_path, str(yr), '*.nc'))
+    file_list.sort()
+    for fname in file_list:
+        print (f'Reading ... {fname}')
+        ncfile = netCDF4.Dataset(fname)
+        bess = np.array(ncfile.variables['surface_downwelling_shortwave_flux_in_air']).T 
+        ncfile.close()
+        bess = bess.astype('float32')
+        bess[bess==-9999] = np.nan
+        points = np.array((lon_bess.flatten(), lat_bess.flatten())).T
+        values = bess.flatten()
+        RSDN = griddata(points, values, (lon_goci, lat_goci), method='linear')
+        print (RSDN)
+        write_fname = f'EA6km_BESS_RSDN_{yr}_{os.path.basename(fname)[-6:-3]}.mat'
+        matlab.savemat(os.path.join(write_path, str(yr), write_fname), {'RSDN':RSDN})
+        print (fname)
+        break
+    print (yr)
 """
 ##  2017 (mat)
 YEARS = [2017]
@@ -54,30 +60,4 @@ for yr in YEARS:
         fname = flist[-5:]
         matlab.savemat(os.path.join(path_data, 'DA_GOCI6km', 'BESS', str(yr)), f'EA6km_BESS_RSDN_{yr}_{fname}.mat', RSDN) 
         print (flist[k])
-
-"""
-    toc
-    %%  전처리
-    % %%lon
-    % a = 89.975;
-    % b = 0;
-    % for i= 1:3600;
-    % a(i,1) = a(1) - b;
-    % b = b + 0.05;
-    % end
-    % for i=1:3600;
-    % lon10 (i,:) = lon(1,:);
-    % end
-    %
-    % %lat
-    % a = -179.975;
-    % b = 0;
-    % for i= 1:7200;
-    % a(1,i) = a(1) + b;
-    % b = b + 0.05;
-    % end
-    %
-    % for i=1:7200;
-    % lat10 (:,i) = lat(:,1);
-    % end
 """
