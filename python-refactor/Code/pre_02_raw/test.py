86efcc4b187ba44dcfdb2b7dc68a535b1a89df1f
Sehyun <sehyun@unist.ac.kr>
2019-12-11 19:30:40 +0900

    add Data/* to .gitignore

---
+++ python-refactor/Code/pre_02_raw/test.py
@@ -0,0 +1,45 @@
+### Package Import
+import sys
+import os
+base_dir = os.environ['GEMS_HOME']
+project_path = os.path.join(base_dir, 'python-refactor')
+sys.path.insert(0, project_path)
+from Code.utils import matlab
+
+import netCDF4
+import numpy as np
+import glob
+from scipy.interpolate import griddata
+
+### Setting path
+data_base_dir = os.path.join('/', 'media', 'sf_GEMS', 'Data')
+raw_data_path = os.path.join(data_base_dir, 'Raw', 'BESS') 
+write_path = os.path.join(data_base_dir, 'Preprocessed_raw', 'BESS')
+
+mat = matlab.loadmat(os.path.join(data_base_dir, 'grid', 'grid_bess.mat')) # lon_bess, lat_bess
+lon_bess = mat['lon_bess']
+lat_bess = mat['lat_bess'] 
+del mat
+
+mat = matlab.loadmat(os.path.join(data_base_dir, 'grid', 'grid_goci.mat')) # lon_goci, lat_goci
+lon_goci = mat['lon_goci']
+lat_goci = mat['lat_goci']
+del mat
+
+yr = 2016
+file_list = glob.glob(os.path.join(raw_data_path, str(yr), '*.nc'))
+file_list.sort()
+fname = file_list[0]
+print (f'Reading ... {fname}')
+ncfile = netCDF4.Dataset(fname)
+bess = np.array(ncfile.variables['surface_downwelling_shortwave_flux_in_air']).T 
+ncfile.close()
+bess = bess.astype('float32')
+bess[bess==-9999] = np.nan
+points = np.array((lon_bess.flatten(), lat_bess.flatten())).T
+values = bess.flatten()
+RSDN = griddata(points, values, (lon_goci, lat_goci), method='linear')
+print (RSDN)
+write_fname = f'EA6km_BESS_RSDN_{yr}_{os.path.basename(fname)[-6:-3]}.mat'
+matlab.savemat(os.path.join(write_path, str(yr), write_fname), {'RSDN':RSDN})
+print (fname)
