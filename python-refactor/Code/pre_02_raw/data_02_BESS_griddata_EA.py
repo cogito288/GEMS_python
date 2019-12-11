38f55352807c134fe9ef264895cb3d1b60776fc0
Sehyun <sehyun@unist.ac.kr>
2019-12-12 00:45:07 +0900

    working on MODIS_02

--- python-refactor/Code/pre_02_raw/data_02_BESS_griddata_EA.py
+++ python-refactor/Code/pre_02_raw/data_02_BESS_griddata_EA.py
@@ -12,7 +12,7 @@ import glob
 from scipy.interpolate import griddata
 
 ### Setting path
-data_base_dir = os.path.join('/', 'media', 'sf_GEMS', 'Data')
+data_base_dir = os.path.join(project_path, 'Data')
 raw_data_path = os.path.join(data_base_dir, 'Raw', 'BESS') 
 write_path = os.path.join(data_base_dir, 'Preprocessed_raw', 'BESS')
 
@@ -26,6 +26,9 @@ lon_goci = mat['lon_goci']
 lat_goci = mat['lat_goci']
 del mat
 
+points = np.array((lon_bess.flatten(), lat_bess.flatten())).T
+del lon_bess, lat_bess
+
 #%% Until 2016 (nc)
 YEARS = [2016]
 for yr in YEARS:
@@ -36,28 +39,12 @@ for yr in YEARS:
         ncfile = netCDF4.Dataset(fname)
         bess = np.array(ncfile.variables['surface_downwelling_shortwave_flux_in_air']).T 
         ncfile.close()
-        bess = bess.astype('float32')
+        bess = bess.astype('float64')
         bess[bess==-9999] = np.nan
-        points = np.array((lon_bess.flatten(), lat_bess.flatten())).T
         values = bess.flatten()
         RSDN = griddata(points, values, (lon_goci, lat_goci), method='linear')
         print (RSDN)
         write_fname = f'EA6km_BESS_RSDN_{yr}_{os.path.basename(fname)[-6:-3]}.mat'
         matlab.savemat(os.path.join(write_path, str(yr), write_fname), {'RSDN':RSDN})
         print (fname)
-        break
     print (yr)
-"""
-##  2017 (mat)
-YEARS = [2017]
-for yr in YEARS:
-    os.chdir(os.path.join(path_bess, str(yr)))
-    flist = matlab.get_files_endswith('.', '.mat')
-
-    for k in range(len(flist)):
-        bess = matlab.loadmat(flist[k])
-        RSDN = griddata(zip(lon_bess, lat_bess), bess, zip(lon_goci, lat_goci), method='linear') # %리샘플링 (0.05 degree를 6km goci 격자로)
-        fname = flist[-5:]
-        matlab.savemat(os.path.join(path_data, 'DA_GOCI6km', 'BESS', str(yr)), f'EA6km_BESS_RSDN_{yr}_{fname}.mat', RSDN) 
-        print (flist[k])
-"""
