45cd5c63a72e276bca71c0cb5d0a49c87d56a79a
Sehyun <sehyun@unist.ac.kr>
2019-12-17 19:24:43 +0900

    MODIS 03 clear

--- python-refactor/Code/pre_01_raw/MODIS_03_LC_to_bin_by_class_nan_water.py
+++ python-refactor/Code/pre_01_raw/MODIS_03_LC_to_bin_by_class_nan_water.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------------
-# LC_to_bin_by_class.py
-# Author: Minso Shin
-# Description: Make binary classification data by class from reclassified data
-# ---------------------------------------------------------------------------
+### Package Import
 import sys
 import os
 base_dir = os.environ['GEMS_HOME']
@@ -11,55 +6,54 @@ project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
 from Code.utils import matlab
 
-import scipy.io as sio
-import numpy as np
+import rasterio as rio
 import glob
-import time
-import h5py 
-import pygrib
-import re
-from osgeo import gdal
-import tempfile
-import subprocess
-#import arcpy
-#from arcpy import env
+import time 
 
-path_modis="\\\\10.72.26.46\\irisnas6\\Data\\MODIS_tile\\02region\\EastAsia\\MCD12Q1\\03masked_N50W110S20E150\\"
-path_data="\\\\10.72.26.56\\irisnas5\\Data\\"
-path_work=path_data+"pre\\MODIS\\MCD12Q1\\"
+data_base_dir = os.path.join(project_path, 'Data')
+path_modis = os.path.join(data_base_dir, 'Preprocessed_raw', 'MODIS')
 class_name = ["forest","shrub","savannas","grass","wetland","crop","urban","snow","barren","water"]
 
-
-YEARS = range(2001, 2018)
+YEARS = [2016]
 for yr in YEARS:
-    #env.workspace=""+path_work+""
-    src_dataset = os.path.join(path_modis, f'm_MODIS_LC_500m_{yr}.tif)
-    dst_dataset = os.path.join(path_work, '01_reclassified', f'reclass_MODIS_LC_500m_EA_{yr}.tif')
-
-    # Process: Reclassify
-    # Reclassify_sa (in_raster, reclass_field, remap, out_raster, missing_values)
-    # in_raster : The input raster to be reclassified.
-    # reclass_field: Field denoting the values that will be reclassified.
-    # remap:  RemapRange and RemapValue
-    #       The remap list is composed of three components: From, To, and New values. Each row in the remap list is separated by a semicolon, and the three components are separated by spaces. For example, "0 5 1;5.01 7.5 2;7.5 10 3".
-    # out_raster: The raster to be created.
-    # Change missing values to NoData (Optional): DATA — A keyword signifying that if any cell location on the input raster contains a value that is not present or reclassed in a remap table, the value should remain intact and be written for that location to the output raster.
-    # arcpy.gp.Reclassify_sa(a, "Value", "1 1;2 1;3 1;4 1;5 1;6 2;7 2;8 3;9 3;10 4;11 5;12 6;13 7;14 6;15 8;16 9;17 10;NODATA 10", b, "DATA")
-    expr = "1*(A==1)+1*(A==2)+1*(A==3)+1*(A==4)+1*(A==5)"
-    expr += "+2*(A==6)+2*(A==7)"
-    expr += "+3*(A==8)+3*(A==9)"
-    expr += "+4*(A==10)+5*(A==11)+5*(A==12)+6*(A==13)"
-    cmd = ["gdal_calc.py", "-A", src_dataset, f"--outfile={dst_dataset}", "--cal", expr]
-    print (cmd)
-    subprocess.call(cmd)
-    expr = None
-
-    for ii in range(0,10):
-        dst_dataset02 = os.path.join(path_work, "02_LC_binary", str(yr), f"MODIS_LC_500m_EA_{class_name[ii]}_{yr}.tif")
-        expr = f"1*(A=={ii+1})"
-        cmd = ["gdal_calc.py", "-A", dst_dataset, f"--outfile={dst_dataset02}", "--cal", expr]
-        #arcpy.gp.RasterCalculator_sa("Con(\""+b+"\"=="+str(ii+1)+",1,0)", c)
-        print (cmd)
-        subprocess.call(cmd)
+    src_dataset = os.path.join(path_modis, '03masked_N50W110S20E150', f'm_MODIS_LC_500m_{yr}.tif')
+    dst_dataset = os.path.join(path_modis, '01_reclassified', f'reclass_MODIS_LC_500m_EA_{yr}.tif')
+    matlab.check_make_dir(os.path.dirname(dst_dataset)) # Debugging
+    matlab.check_make_dir(os.path.join(path_modis, '02_LC_binary', str(yr))) # Debugging
+    
+    with rio.open(src_dataset) as src:
+        band = src.read(1).copy()
+        band[(band>=1) & (band<6)] = 1
+        band[(band>=6) & (band<8)] = 2
+        band[(band>=8) & (band<10)] = 3
+        band[band==10] = 4
+        band[band==11] = 5
+        band[(band==12) | (band==14)] = 6
+        band[band==13] = 7
+        band[band==15] = 8
+        band[band==16] = 9
+        band[(band==17) | (band==src.meta['nodata'])] = 10
         
-
+        kwargs = src.meta.copy()
+        kwargs.update({
+                'nodata':None,
+                'compress':'LZW',
+        })
+        with rio.open(dst_dataset, 'w', **kwargs) as dst:
+            dst.write_band(1, band)
+    print (os.path.basename(dst_dataset))
+    
+    with rio.open(dst_dataset) as dst:
+        band = dst.read(1).copy()
+        for ii, col in enumerate(class_name):
+            dst_dataset02 = os.path.join(path_modis, "02_LC_binary", str(yr), f"MODIS_LC_500m_EA_{col}_{yr}.tif")
+            band2 = band.copy()
+            band2[band2==(ii+1)] = 1
+            band2[band2!=1] = 0
+            kwargs = dst.meta.copy()
+            kwargs.update({
+                    'compress':'LZW',
+            })
+            with rio.open(dst_dataset02, 'w', **kwargs) as dst02:
+                dst02.write_band(1, band2)
+            print (os.path.basename(dst_dataset02))
