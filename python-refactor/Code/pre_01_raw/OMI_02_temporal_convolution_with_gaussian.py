6d2678e43fd23249af7b3b2d1f79f63469f25339
msshin1125 <58220396+msshin1125@users.noreply.github.com>
2019-12-28 01:17:19 +0900

    Update OMI_02_temporal_convolution_with_gaussian.py

--- python-refactor/Code/pre_01_raw/OMI_02_temporal_convolution_with_gaussian.py
+++ python-refactor/Code/pre_01_raw/OMI_02_temporal_convolution_with_gaussian.py
@@ -15,9 +15,8 @@ from numba import njit
 data_base_dir = os.path.join('/', 'share', 'irisnas5', 'GEMS', 'GEMS_python')
 path_read = os.path.join(data_base_dir, 'Preprocessed_raw', 'OMI_tempConv')
 
-
 ### Setting period
-YEARS = [2016] #, 2018, 2019
+#YEARS = [2016] #, 2018, 2019
 pname_list = ['OMNO2d','OMSO2e_m','OMDOAO3e_m','OMHCHOG']
 
 mask = np.zeros((720, 1440))
@@ -28,12 +27,12 @@ mask = mask.ravel(order='F')
 for pname in pname_list:
     print (pname)
     ### Load data
-    YEARS = [2016]
+    YEARS = [2014,2015,2016,2017,2018]
     def read_and_mask(yr):
         if os.path.isfile(os.path.join(path_read, f'{pname}_{yr}_DU.mat')):
             data_yr = matlab.loadmat(os.path.join(path_read, f'{pname}_{yr}_DU.mat'))['data_yr']
         elif os.path.isfile(os.path.join(path_read, f'{pname}_trop_CS_{yr}_DU.mat')):
-            data_yr = matlab.loadmat(os.path.join(path_read, f'{pname}_{yr}_DU.mat'))['data_yr']
+            data_yr = matlab.loadmat(os.path.join(path_read, f'{pname}_trop_CS_{yr}_DU.mat'))['data_yr']
         elif os.path.isfile(os.path.join(path_read, f'{pname}_{yr}.mat')):
             data_yr = matlab.loadmat(os.path.join(path_read, f'{pname}_{yr}.mat'))['data_yr']
         else:
@@ -49,10 +48,10 @@ for pname in pname_list:
     def calculate(data):
         data_conv = np.full(data.shape, np.nan)
         for k in range(data.shape[0]):
-            for t in range(5478): # for 15 years
+            for t in range(data.shape[1]):
                 t_ = t+1
                 w_sum = 0; x_sum = 0;
-                for n in range(5478): # for 15 years
+                for n in range(data.shape[1]):
                     n_ = n+1
                     if np.isnan(data[k,n])==0:
                         w = np.exp((-(t_-n_)**2)/(2*sigma**2)) #  calcuate weights using t,n(days) with sigma # (k+1)-(n-1)=k-n
@@ -66,7 +65,7 @@ for pname in pname_list:
             print (k)
         return data_conv
     data_conv = calculate(data)
-    matlab.savemat(os.path.join(path_read, f'tempConv_{pname}_sigma{sigma}_2005_2019.mat'), 
+    matlab.savemat(os.path.join(path_read, f'tempConv_{pname}_sigma{sigma}_{YEARS[0]}_{YEARS[len(YEARS)-1]}.mat'), 
                    {'data_conv':data_conv, 'data':data})
     #matlab.savemat(os.path.join(path_read, f'tempConv_{pname}_trop_CS_sigma{sigma}_2005_2019.mat'),
     #                {'data_conv':data_conv, 'data':data})
