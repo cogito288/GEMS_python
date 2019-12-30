30ac1fa9e1630bab9ce0d7859629c99f0e9c4cc6
Sehyun <sehyun@unist.ac.kr>
2019-12-31 01:37:16 +0900

    done CN_station 03

--- python-refactor/Code/pre_03_station/CN_station_03_read_raw_outlier.py
+++ python-refactor/Code/pre_03_station/CN_station_03_read_raw_outlier.py
@@ -10,6 +10,7 @@ import copy
 import numpy as np
 import pandas as pd
 import glob
+import h5py
 
 ### Setting path
 data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
@@ -19,31 +20,26 @@ path_station = os.path.join(data_base_dir, 'Preprocessed_raw', 'Station')
 stn_info_cn = pd.read_csv(os.path.join(path_station, 'Station_CN', 'cn_stn_code_lonlat_period.csv'))
 stn_info_cn = stn_info_cn.values
 ## stn_scode_data for China
-header_ndata = ['doy','yr','mon','day','CST','AQI','PM25','PM25_24h',
+header_ndata = np.array(['doy','yr','mon','day','CST','AQI','PM25','PM25_24h',
     'PM10','PM10_24h','SO2','SO2_24h','NO2','NO2_24h','O3','O3_24h',
-    'O3_8h','O3_8h_24h','CO','CO_24h','scode','scode2']
+    'O3_8h','O3_8h_24h','CO','CO_24h','scode','scode2'], dtype=h5py.string_dtype(encoding='utf-8'))
 
 YEARS = [2016] # range(2015, 2019+1)
 for yr in YEARS:
     fname = f'stn_code_data_rm_outlier_{yr}.mat'
     ndata = matlab.loadmat(os.path.join(path_station,'Station_CN', 'stn_code_data', fname))['stn_CN']
     ndata = np.hstack([ndata, np.zeros([ndata.shape[0], 1])])
-    print (ndata[
+    
     ndata_scode = None
     # Assign scode2
     for j in range(stn_info_cn.shape[0]):
         ndata_temp = ndata[ndata[:,-2]==stn_info_cn[j,0],:]
         for k in range(1,5+1): 
             ndata_temp2 = ndata_temp[ndata_temp[:,2]==k,:]
-            print (ndata_temp2.shape)
-            
-            print (ndata_temp2[:10, 17])
-            print (ndata_temp2[:10, 18])
-            sys.exit()
             if len(ndata_temp2)!=0:
                 yrmon = yr*100+k
                 idx = (stn_info_cn[j,4]<=yrmon)&(stn_info_cn[j,5]>=yrmon)
-                if idx==1:
+                if idx: # True
                     ndata_temp2[:,-1]=stn_info_cn[j,1]
                     if ndata_scode is None:
                         ndata_scode = ndata_temp2
@@ -54,20 +50,24 @@ for yr in YEARS:
             matlab.savemat(os.path.join(path_station,'Station_CN', fname), {'ndata_scode':ndata_scode})
             ndata_scode = None
         elif (j+1)==stn_info_cn.shape[0]:
-            fname = f'stn_scode_data_{yr}_1501.mat'
-            matlab.savemat(os.path.join(path_station,'Station_CN', fname), {'ndata_scode':ndata_scode})
-        print (f'{j} / {stn_info_cn.shape[0]}')
-
+            if ndata_scode is not None:
+                fname = f'stn_scode_data_{yr}_1501.mat'
+                matlab.savemat(os.path.join(path_station,'Station_CN', fname), {'ndata_scode':ndata_scode})
+        print (f'{j+1} / {stn_info_cn.shape[0]}')
+    
     ndata_scode = None
-    for k in range(1, 1501+1, 100):
-        fname = f'stn_scode_data_{yr}_{k:04d}.mat'
-        ndata_scode_temp = matlab.loadmat(os.path.join(path_station,'Station_CN', fname))['ndata_scode']
+    file_list = glob.glob(os.path.join(path_station,'Station_CN', f'stn_scode_data_{yr}_*.mat'))
+    file_list.sort()
+    for fname in file_list:
+        print (f'Reading ... {os.path.basename(fname)}')
+        ndata_scode_temp = matlab.loadmat(fname)['ndata_scode']
+        print (ndata_scode_temp.shape)
         if ndata_scode is None:
             ndata_scode = ndata_scode_temp
         else:
-            print (ndata_scode.shape)
-            print (ndata_scode_temp.shape)
             ndata_scode = np.vstack([ndata_scode, ndata_scode_temp])
     fname = f'cn_stn_scode_data_rm_outlier_{yr}.mat'
-    matlab.savemat(os.path.join(path_station,'Station_CN','stn_scode_data', fname), {'ndata_scode':ndata_scode,'header_ndata':header_ndata})
+    matlab.savemat(os.path.join(path_station,'Station_CN','stn_scode_data', fname), {'ndata_scode':ndata_scode})
+    with h5py.File(os.path.join(path_station,'Station_CN','stn_scode_data', fname), 'a') as dst:
+        dst['header_ndata'] = header_ndata
     print (yr)
