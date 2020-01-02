467102e706eda1c1de90810a65de02a38cfda9e9
msshin1125 <58220396+msshin1125@users.noreply.github.com>
2020-01-02 22:44:06 +0900

    Update JP_station_01_01_read_raw.py

--- python-refactor/Code/pre_03_station/JP_station_01_01_read_raw.py
+++ python-refactor/Code/pre_03_station/JP_station_01_01_read_raw.py
@@ -1,7 +1,8 @@
 ### Package Import
 import sys
 import os
-base_dir = os.environ['GEMS_HOME']
+#base_dir = os.environ['GEMS_HOME']
+base_dir = 'D:\github\GEMS_python'
 project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
 from Code.utils import matlab
@@ -12,98 +13,67 @@ import pandas as pd
 import glob
 
 ### Setting path
-data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
+#data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
+data_base_dir = os.path.join('//', '10.72.26.56','irisnas5', 'GEMS', 'GEMS_python')
+#data_base_dir = os.path.join('/', 'share', 'irisnas5', 'GEMS', 'GEMS_python')
+path_in_situ = os.path.join('/','share','irisnas6','Data','In_situ')
 path_station = os.path.join(data_base_dir, 'Preprocessed_raw', 'Station') 
 
 # header = {'doy','year','month','day','KST','SO2','CO','O3','NO2','PM10','PM25','scode'}
 pcode = [['01','SO2','ppb'],
-         ['02','NO','ppb'], 
          ['03','NO2','ppb'],
-         ['04','NOX','ppb'],
          ['05','CO','x01ppm'],
          ['06','OX','ppb'],
-         ['07','NMHC','x10ppbc'],
-         ['08','CH4','x10ppbc'],
-         ['09','THC','x10ppbc'],
          ['10','SPM','ug_m3'],
-         ['12','PM25','ug_m3'],
-         ['21','WD','x16DIRC'],
-         ['22','WS','x01m_s'],
-         ['23','TEMP','x01degC'],
-         ['24','HUM','percent'],
-         ['25','SUN','x001MJ'],
-         ['26','RAIN','mm'],
-         ['27','UV','x001MJ'],
-         ['28','PRS','mb'],
-         ['29','NETR','x001MJ'],
-         ['41','CO2','x01ppm'],
-         ['42','O3','ppb']]
-
-# '43','HCL'; '44','HF'; '45','H2S'; '46','SHC'; '47','UHC'
+         ['12','PM25','ug_m3']]
 
 # p=0; varname = 'SO2'
-# p=1; varname = 'NO'
 # p=2; varname = 'NO2'
-# p=3; varname = 'NOX'
 # p=4; varname = 'CO'
 # p=5; varname = 'OX'
-# p=6; varname = 'NMHC'
-# p=7; varname = 'CH4'
-# p=8; varname = 'THC'
 # p=9; varname = 'SPM'
 # p=11; varname = 'PM25'; p=50
-# p=20; varname = 'WD'
-# p=21; varname = 'WS'
-# p=22; varname = 'TEMP'
-# p=23; varname = 'HUM'
-# p=24; varname = 'SUN'
-# p=25; varname = 'RAIN'
-# p=26; varname = 'UV'
-# p=27; varname = 'PRS'
-# p=28; varname = 'NETR'
-# p=40; varname = 'CO2'
-# p=41; varname = 'O3'
-varname = 'PM25'; p=50
 
-header_p = ['doy','year','month','day','scode','ccode','KST', 'stn_{varname}']
-# '측정년도/측정국코드/시도코드/측정항목코드/측정단위코드/측정월/측정일'
+for p,varname,unit in pcode:
+    header_p = ['doy','year','month','day','scode','ccode','KST',f'stn_{varname}']
+    # 'yr/scode/ccode/pcode/unit/month/day'
 
-YEARS = [2016] # range(2009, 2009+1)
-for yr in YEARS:
-    file_list = glob.glob(os.path.join(path_station, 'Station_JP', str(yr), f'*_{p:02d}.txt'))
-    for fname in file_list:
-        data_temp = pd.read_csv(fname)
-        if data is None:
-            data = data_temp
-        else:
-            data = pd.concat([data, data_temp])
-        print (data)
-    if varname == 'PM25': #Need to check
-        vv = data[:, data.columns[3]]
-        idx_PM25 = vv.isin(['PM25'])
-        idx_PMBH = vv.isin(['PMBH'])
-        idx_PMFL = vv.isin(['PMFL'])
-        data_PMBH = data[idx_PMBH]
-        data_PMFL = data[idx_PMFL]
-        data = data[idx_PM25,:]
-
-    data = np.delete(data, [3,4], axis=1)
-    data = data.values
-    yrmonday = data[:,0]*10000 + data[:,3]*100 + data[:,4]
-    data_datenum = matlab.datenum(str(yrmonday))
-    doy_000 = matlab.datenum(f'{yr}00000')
-    data_doy = data_datenum-doy_000
-
-    data_info = np.hstack([data_doy,data[:,0],data[:,3],data[:,4],data[:,1:3]]) # 'doy','year','month','day','scode','ccode'
-    data_new = None
-    for KST in range(1,24+1):
-        data_temp = data_info
-        data_temp[:,6]=KST
-        data_temp = np.hstack([data_temp, data[:,4+KST]])
-        if data_new is None:
-            data_new = data_temp
-        else:
-            data_new= np.vstack([data_new, data_temp])
-    vars()[f'stn{varname}_tbl'] = pd.DateFrame(data_new, columns=header_p) 
-    matlab.savemat(os.path.join(path_station, 'Station_JP', f'JP_stn{varname}_{yr}.mat'), 
-                   {col: vars()[f'stn{varname}_tbl'][col].values for col in vars()[f'stn{varname}_tbl'].columns})
+    YEARS = [2016] # range(2009, 2009+1)
+    for yr in YEARS:
+        file_list = glob.glob(os.path.join(path_in_situ, 'Station_JP', str(yr), f'*_{p:02d}.txt'))
+        for fname in file_list:
+            data_temp = pd.read_csv(fname)
+            if data is None:
+                data = data_temp
+            else:
+                data = pd.concat([data, data_temp])
+            print (data)
+        if varname == 'PM25': #Need to check
+            vv = data[:, data.columns[3]]
+            idx_PM25 = vv.isin(['PM25'])
+            idx_PMBH = vv.isin(['PMBH'])
+            idx_PMFL = vv.isin(['PMFL'])
+            data_PMBH = data[idx_PMBH]
+            data_PMFL = data[idx_PMFL]
+            data = data[idx_PM25,:]
+    
+        data = np.delete(data, [3,4], axis=1)
+        data = data.values
+        yrmonday = data[:,0]*10000 + data[:,3]*100 + data[:,4]
+        data_datenum = matlab.datenum(str(yrmonday))
+        doy_000 = matlab.datenum(f'{yr}00000')
+        data_doy = data_datenum-doy_000
+    
+        data_info = np.hstack([data_doy,data[:,0],data[:,3],data[:,4],data[:,1:3]]) # 'doy','year','month','day','scode','ccode'
+        data_new = None
+        for KST in range(1,24+1):
+            data_temp = data_info
+            data_temp[:,6]=KST
+            data_temp = np.hstack([data_temp, data[:,4+KST]])
+            if data_new is None:
+                data_new = data_temp
+            else:
+                data_new= np.vstack([data_new, data_temp])
+        vars()[f'stn{varname}_tbl'] = pd.DateFrame(data_new, columns=header_p) 
+        matlab.savemat(os.path.join(path_station, 'Station_JP', f'JP_stn{varname}_{yr}.mat'), 
+                       {col: vars()[f'stn{varname}_tbl'][col].values for col in vars()[f'stn{varname}_tbl'].columns})
