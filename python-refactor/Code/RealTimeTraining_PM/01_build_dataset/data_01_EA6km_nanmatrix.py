0988413ca7eff81070bfb3bb6f68391905242044
Sehyun <sehyun@unist.ac.kr>
2020-01-27 21:48:11 +0900

    done training 01 data_01

--- python-refactor/Code/RealTimeTraining_PM/01_build_dataset/data_01_EA6km_nanmatrix.py
+++ python-refactor/Code/RealTimeTraining_PM/01_build_dataset/data_01_EA6km_nanmatrix.py
@@ -13,31 +13,34 @@ import glob
 
 ### Setting path
 data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
-path_preprocessed = os.path.join(data_base_dir, 'Preprocessed_raw')
+path_ea_goci = os.path.join(data_base_dir, 'Preprocessed_raw', 'EA_GOCI6km') 
 
 ##
-YEARS = [2015, 2016]
+YEARS = [2016]
 for yr in YEARS:
-    if yr ==2016: days = 366
-    else: days = 365
+    if yr%4==0: days= 366
+    else: days=365
+        
+    nanidx = np.zeros((218999, days, 7)) # python needs to predefine and does not allow size-growing up directly. 
     for doy in range(1, days+1):
         for utc in range(7+1):
             fname = f'cases_EA6km_{yr}_{doy:03d}_{utc:02d}.mat'
-            mat = matlab.loadmat(os.path.join(path_preprocessed,'EA_GOCI6km/cases_mat', fname))
-            data_tbl = pd.DataFrame(
-            data = data_tbl.values
-            data = data[:,[3:19,27:31,34:37]] # extract specific variables for PMs
-            del data_tbl
-            
+            mat = matlab.loadmat(os.path.join(path_ea_goci, 'cases_mat', str(yr), fname))
+            # data[:,[3:19,27:31,34:37]] # extract specific variables for PMs
+            tmp_cols = ['OMHCHOG_tc', 'AOD', 'AE', 'FMF', 'SSA', 'NDVI', 'RSDN', 'Precip', 'DEM', 'Temp', 'Dew', 'RH', 'P_srf', 'MaxWS', 'PBLH', 'Visibility', 'stack1_maxWS', 'stack3_maxWS', 'stack5_maxWS', 'stack7_maxWS', 'AP3h', 'DOY', 'PopDens']
+            df = pd.DataFrame(columns=tmp_cols)
+            for col in tmp_cols:
+                df[col] = np.squeeze(mat[col])
+            data = df.values 
+            del df, mat
             data[np.isnan(data)] = -9999 # convert nan ro -9999
-            nanidx[:,doy-1,utc] = data[:,1] # make a matrix
-            idy, idx = np.where[data == -9999] # np.where the location where the -9999 is
+            nanidx[:,doy-1,utc] = data[:,0] # make a matrix
+            idy, idx = np.where(data == -9999) # np.where the location where the -9999 is
             idy = np.unique(idy) # remove same number
-            nanidx[idy.flatten(), doy-1, utc] = np.nan # change the value to nan value 
+            nanidx[idy.ravel(order='F'), doy-1, utc] = np.nan # change the value to nan value 
             nanidx[:,doy-1,utc]=np.isnan(nanidx[:,doy-1,utc]) # np.where where is nan value(nan=1, valied value=0)
             print (utc)
         print (doy)
     fname = f'nanidx_EA6km_{yr}.mat'
-    matlab.savemat(os.path.join(path_nas6,'EA_GOCI6km'), fname, {'nanidx':nanidx})
+    matlab.savemat(os.path.join(path_nas6,'EA_GOCI6km', fname), {'nanidx':nanidx})
     print (yr)
-
