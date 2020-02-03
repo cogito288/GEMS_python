734fdc5b4e6624dfa0ba36f72b1dec04992c5605
msshin1125 <58220396+msshin1125@users.noreply.github.com>
2020-02-04 01:51:25 +0900

    KR_station

--- python-refactor/Code/pre_03_station/KR_station_01_read_raw.py
+++ python-refactor/Code/pre_03_station/KR_station_01_read_raw.py
@@ -2,205 +2,87 @@
 import sys
 import os
 base_dir = os.environ['GEMS_HOME']
+#base_dir = 'D:\github\GEMS_python'
 project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
 from Code.utils import matlab
 
-import scipy.io as sio
-import numpy as np
-import glob
+import copy
+import numpy as np 
 import pandas as pd
+import glob
 
 ### Setting path
-data_base_dir = os.path.join('/', 'media', 'sf_GEMS_1', 'Data')
-raw_data_path = os.path.join(data_base_dir, 'Raw', 'GOCI_AOD') 
-write_path = os.path.join(data_base_dir, 'Preprocessed_raw', 'GOCI_AOD')
-path_insiu = '//10.72.26.46/irisnas6/Data/In_situ/AirQuality_SouthKorea/'
-path = '//10.72.26.56/irisnas5/Data/Station/Station_Korea/'
-# path_data = '/share/irisnas2/Data/Aerosol/'
-# path = '/share/irisnas5/Data/Station/Station_Korea/'
-# addpath(genpath('/share/irisnas5/Data/matlab_func/'))
+# data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
+# data_base_dir = os.path.join('//', '10.72.26.56','irisnas5', 'GEMS', 'GEMS_python')
+data_base_dir = os.path.join('/', 'share', 'irisnas5', 'GEMS', 'GEMS_python')
+# path_in_situ = os.path.join(data_base_dir, 'Raw')
+# path_in_situ = os.path.join('//','10.72.26.46','irisnas6','Data','In_situ')
+path_in_situ = os.path.join('/','share','irisnas6','Data','In_situ')
+path_station = os.path.join(data_base_dir, 'Preprocessed_raw', 'Station') 
+path_stn_kr = os.path.join(path_station, 'Station_KR')
 
-## 2005-2013 (xlsx, PM2.5없음)
-YEARS = range(2005, 2013+1)
+cols=['측정소코드','측정일시','SO2','CO','O3','NO2','PM10','PM25']
+
+## 
+YEARS = range(2014, 2018+1)
 for yr in YEARS:
-    data = []
-    for i in range(1, 4+1):
-        fname = f'{yr}년{i:02d}분기.xlsx'
-        tmp_df = pd.read_excel(os.path.join(path_insiu,str(yr), fname))
-        text_cols = []
-        num_cols = []
-        for col in tmp_df.columns:
-            if tmp_df[col].dtype.name == 'object':
-                text_cols.append(col)
-            else:
-                text_cols.append(num_cols)
-        txt = tmp_df[text_cols]
-        data_temp = txt[1:,2:9]
-        data = pd.concat([data, data_temp], axis=0)
+    flist = glob.glob(os.path.join(path_in_situ,'AirQuality_SouthKorea',str(yr),'*'))
+    flist.sort()
+    data = None
+    for fname in flist:
+        fname_body, fname_ext = os.path.splitext(fname)
+        if fname_ext=='.csv':
+            try:
+                tmp_df = pd.read_csv(fname)
+            except:
+                tmp_df = pd.read_csv(fname,encoding='euc-kr')
+        else:
+            tmp_df = pd.read_excel(fname)
+
+        data_temp = tmp_df.loc[:,cols]
+        if data is None:
+            data = data_temp
+        else:
+            data = pd.concat([data, data_temp])
+        del tmp_df, data_temp
+    data = data.values
+    data[data<0]=np.nan
+
+    dstr = [str(int(x)) for x in data[:,1]]
+    tmp_df = pd.DataFrame(dstr, columns=['time'])
+    pat = '(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})(?P<hour>\d{2})'
+    dvec = pd.to_datetime(tmp_df['time'].str.extract(pat, expand=True))
+    dstr = dvec.dt.strftime('%Y%m%d').values
+    data_datenum = [matlab.datenum(val) for val in dstr]
+    doy_000 = matlab.datenum(f'{yr}00000')
+    data_doy = np.array([np.floor(val-doy_000) for val in data_datenum])
+    dvec = [(val.year, val.month, val.day, val.hour) for val in dvec]
+    dvec = np.array(dvec)
+    ndata = np.hstack([data_doy.reshape(-1,1),dvec,data[:,2:],data[:,0].reshape(-1,1)])
+    # doy, yr, mon, day, time, SO2, CO, O3, NO2, PM10, PM25, scode
+    matlab.savemat(os.path.join(path_stn_kr,'stn_code_data',f'stn_code_data_{yr}.mat'), 
+                   {'ndata':ndata})
     
-    dvec = pd.to_datetime(data[:,1], format='%Y%m%d%H') #    dvec = datevec(data(:,2),'yyyymmddHH')
-    data_datenum = matlab.datenum(data[:,1]) # 'yyyymmddHH'
-    doy_000 = matlab.datenum(f'{yr}00000')
-    data_doy = np.floor(data_datenum-doy_000)
-
-    data = np.float64(data)
-    data[data<0]=np.nan
-
-    ndata = np.concatenate((data_doy,dvec(:,1:4),data(:,3:),np.full(data_doy.shape, np.nan),data(:,1)), axis=1)
-    # doy, yr, mon, day, time, SO2, CO, O3, NO2, PM10, (PM25), scode
-
-    fname = f'stn_code_data_{yr}.mat'
-    matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
-
-
-
-## 2014-2016 (csv, PM2.5 컬럼 있음)
-YEARS = range(2014, 2016+1)
-for yr in YEARS:
-    data = []
-    for i in range(1, 4+1):
-        fname = f'{yr}년{i:02d}분기.xlsx'
-        tmp_df = pd.read_excel(os.path.join(path_insiu,str(yr), fname))
-        text_cols = []
-        num_cols = []
-        for col in tmp_df.columns:
-            if tmp_df[col].dtype.name == 'object':
-                text_cols.append(col)
-            else:
-                text_cols.append(num_cols)
-        txt = tmp_df[text_cols]
-        data_temp = tmp_df[2:,[2,4:10]]
-        data = pd.concat([data, data_temp], axis=0)
-
-
-    data = np.float64(data.values)
-    data[data<0]=np.nan
-
-    dstr = [str(x) for x in data[:,1]]
-    dvec = pd.to_datetime(dstr,format='%Y%m%d%H')
-    data_datenum = matlab.datenum(dstr)
-    doy_000 = matlab.datenum(f'{yr}00000')
-    data_doy = np.floor(data_datenum-doy_000)
-
-    ndata = np.concatenate((data_doy,dvec[:,:4],data[:,2:],data[:,0]), axis=1)
-    # doy, yr, mon, day, time, SO2, CO, O3, NO2, PM10, (PM25), scode
-
-    fname = f'stn_code_data_{yr}.mat'
-    matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
-
-## 2017-2018 2분기 (xlsx, PM2.5 있음)
-YEARS = [2018]
-for yr in YEARS:
-    data = []
-    for i in range(1, 4+1):
-        fname = f'{yr}년{i:02d}분기.xlsx'
-        tmp_df = pd.read_excel(os.path.join(path_insiu,str(yr), fname))
-        text_cols = []
-        num_cols = []
-        for col in tmp_df.columns:
-            if tmp_df[col].dtype.name == 'object':
-                text_cols.append(col)
-            else:
-                text_cols.append(num_cols)
-        txt = tmp_df[text_cols]
-        
-        data_temp = tmp_df[1:,[1,3:10]]
-        data = pd.concat([data, data_temp), axis=0)
-
-
-    data_1 = np.float64(data[:,1])
-    data_2 = np.asarray(data[:, 1:])
-    data = np.concatenate((data_1, data_2), axis=1)
-    data[data<0]=np.nan
-
-    dstr = [str(x) for x in data[:,2]]
-    dvec = pd.to_datetime(dstr,format='%Y%m%d%H')
-    data_datenum = matlab.datenum(dstr)
-    doy_000 = matlab.datenum(f'{yr}00000')
-    data_doy = np.floor(data_datenum-doy_000)
-
-    ndata = np.concatenate((data_doy,dvec[:,:4],data[:,2:],data[:,0]), axis=1)
-    # doy, yr, mon, day, time, SO2, CO, O3, NO2, PM10, (PM25), scode
-
-    fname = f'stn_code_data_{yr}.mat'
-    matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
-
-
-## 2018 3분기 - 4분기 (xlsx, PM2.5있음.. 근데 중간에 망 정보가 들어가면서 컬럼 위치가 변경됨)
-i=4
-fname = f'{yr}년{i:02d}분기.xlsx'
-tmp_df = pd.read_excel(os.path.join(path_insiu,str(yr), fname))
-data_temp = tmp_df[1:,[2,4:11]]
-data = pd.concat([data, data_temp], axis=0)
-
-## 201810_201904 
-data = []
-tmp_df = pd.read_excel(os.path.join(path_insiu,'201810_201904', '201810_201904_pt1.csv'))
-data_temp = tmp_df[1:,[1,5:6,9,8,7,10,11]]
-data = pd.concat([data, data_temp], axis=0)
-
-tmp_df = pd.read_excel(os.path.join(path_insiu,'201810_201904', '201810_201904_pt2.csv'))
-data_temp = tmp_df[1:,[1,5:6,9,8,7,10,11]]
-data = pd.concat([data, data_temp], axis=0)
-
-data = data.values
-data[data<0]=np.nan
-
-dstr = [str(x) for x in data[:,1]]
-dvec = pd.to_datetime(dstr,format='%Y%m%d%H')
-yr=2019
-yr_idx = dvec[:,0]==yr
-dvec=dvec[yr_idx,:]
-dstr=dstr[yr_idx,:]
-data=data[yr_idx,:]
-data_datenum = matlab.datenum(dstr) 
-
-doy_000 = datenum(f'{yr}00000')
-data_doy = np.floor(data_datenum-doy_000)
-
-ndata = np.concatenate((data_doy,dvec[:,:4],data[:,2:],data[:,0), axis=1)
-# doy, yr, mon, day, time, SO2, CO, O3, NO2, PM10, (PM25), scode
-
-fname = f'stn_code_data_{yr}.mat'
-matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
-
 ## 12월 31일 24시 -> 1월 1일 00시 
-path = '/share/irisnas5/Data/Station/Station_Korea/'
-
-yr=2005
-fname = f'stn_code_data_{yr}.mat'
-matlab.loadmat(os.path.join(path,'stn_code_data', fname))
-data_mv = ndata[ndata[:,1]==(yr+1),:]
-doy_unq = np.unique(data_mv[:,0])
-
-# if length(doy_unq)==1
-data_mv[:,0]=1
-# else
-#     stop
-# 
-
-ndata[ndata[:,1]==(yr+1),:]=[]
-fname = f'stn_code_data_{yr}.mat'
-matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
-
-YEARS = [2016, 2017]
-for yr in YEARS: #2006:2017
+for yr in YEARS:
     fname = f'stn_code_data_{yr}.mat'
-    matlab.loadmat(os.path.join(path,'stn_code_data', fname)
-    ndata = np.concatenate((data_mv, ndata), axis=0)
+    ndata = matlab.loadmat(os.path.join(path_stn_kr,'stn_code_data', fname))['ndata']
+    if yr!=YEARS[0]:
+        fname_mv = f'stn_code_data_{yr}_001_00.mat'
+        data_mv = matlab.loadmat(os.path.join(path_stn_kr,'stn_code_data', fname_mv))['data_mv']
+        ndata = np.vstack([data_mv, ndata])
+        del data_mv
     data_mv = ndata[ndata[:,1]==(yr+1),:]
     doy_unq = np.unique(data_mv[:,0])
 
     if len(doy_unq)==1:
         data_mv[:,0]=1
-        ndata[ndata[:,1]==(yr+1),:]=[]
-
-        fname = f'stn_code_data_{yr}.mat'
-        matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'ndata':ndata})
+        idx= ndata[:,1]==(yr+1)
+        ndata = ndata[~idx]
+        matlab.savemat(os.path.join(path_stn_kr,'stn_code_data', fname), {'ndata':ndata})
     else:
-        # stop
         raise ValueError('len(doy_unq)!=1')
 
-    fname = f'stn_code_data_{yr}_001_00.mat'
-    matlab.savemat(os.path.join(path,'stn_code_data'), fname, {'data_mv':data_mv})
+    fname_mv2 = f'stn_code_data_{yr+1}_001_00.mat'
+    matlab.savemat(os.path.join(path_stn_kr,'stn_code_data', fname_mv2), {'data_mv':data_mv})
--- python-refactor/Code/pre_03_station/KR_station_02_scode2.py
+++ python-refactor/Code/pre_03_station/KR_station_02_scode2.py
@@ -1,57 +1,58 @@
 ### Package Import
 import sys
 import os
-base_dir = os.environ['GEMS_HOME']
+#base_dir = os.environ['GEMS_HOME']
+base_dir = 'D:\github\GEMS_python'
 project_path = os.path.join(base_dir, 'python-refactor')
 sys.path.insert(0, project_path)
 from Code.utils import matlab
 
-import scipy.io as sio
+import copy
 import numpy as np
-import glob
 import pandas as pd
+import glob
+import h5py
 
 ### Setting path
-data_base_dir = os.path.join('/', 'media', 'sf_GEMS_1', 'Data')
-raw_data_path = os.path.join(data_base_dir, 'Raw', 'GOCI_AOD') 
-write_path = os.path.join(data_base_dir, 'Preprocessed_raw', 'GOCI_AOD')
-# path_data = '//10.72.26.56/irisnas5/Data/'
-path_data = '/share/irisnas5/Data/'
-path_stn_kor = [path_data,'Station/Station_Korea/']
-#addpath(genpath('/share/irisnas5/Data/matlab_func/'))
+# data_base_dir = os.path.join('/data2', 'sehyun', 'Data')
+data_base_dir = os.path.join('//', '10.72.26.56','irisnas5', 'GEMS', 'GEMS_python')
+# data_base_dir = os.path.join('/', 'share', 'irisnas5', 'GEMS', 'GEMS_python')
+path_station = os.path.join(data_base_dir, 'Preprocessed_raw', 'Station') 
+path_stn_kr = os.path.join(path_station, 'Station_KR')
 
-stn_info = pd.read_csv(os.path.join(path_data,'Station/Station_Korea/stn_code_lonlat_period_2005_201904.csv'),header=1)
-# scode1, scode2, lon, lat, op_start, op_
+stn_info = pd.read_csv(os.path.join(path_stn_kr, 'stn_code_lonlat_period_2005_201904.csv'))
+stn_info = stn_info.values
+# scode1, scode2, lon, lat, installation, abolation
 
 ## read raw files
-header_ndata = ['doy','yr','mon','day','KST','SO2','CO','O3','NO2','PM10','PM25','scode','scode2']
-
+header_ndata = np.array(['doy','yr','mon','day','KST','SO2','CO','O3','NO2','PM10','PM25','scode','scode2'], 
+                        dtype=h5py.string_dtype(encoding='utf-8'))
 ## stn_scode_data for South Korea
-YEARS = [2008, 2009]
-for yr in YEARS: #=2008:2009 #2005:2019
+YEARS = [2016]
+for yr in YEARS: 
     if yr==2019:
-        ndata=matlab.loadmat([path_stn_kor,'stn_code_data/stn_code_data_2019_010100_042300.mat'])
+        ndata=matlab.loadmat(os.path.join(path_stn_kr,'stn_code_data', 'stn_code_data_2019_010100_042300.mat'))['ndata']
     else:
-        ndata=matlab.loadmat([path_stn_kor,'stn_code_data/', f'stn_code_data_{yr}.mat'])
-    
-        
-    ndata[:,12]=0 # add column for scode2
-    ndata_scode = []
-    ndata[ndata<0]np.nan
-    
-# Assign scode2
-for j in range(stn_info.shape[0]): 
-    ndata_temp = ndata[ndata[:,11]==stn_info[j,0],:]
-    for k in range(1,12+1):
-        ndata_temp2 = ndata_temp[ndata_temp[:,3]==(k-1),:]
-        if np.all(ndata_temp2)==0:
-            yrmon = yr*100+k
-            idx = (stn_info[j,4]<=yrmon)&(stn_info[j,5]>yrmon)
-            if idx==1:
-                ndata_temp2[:,12]=stn_info[j,1]
-                ndata_scode=np.concatenate((ndata_scode,ndata_temp2), axis=0)
+        ndata=matlab.loadmat(os.path.join(path_stn_kr,'stn_code_data', f'stn_code_data_{yr}.mat'))['ndata']
+    ndata = np.hstack([ndata, np.zeros([ndata.shape[0], 1])]) # add column for scode2
+    ndata[ndata<0] = np.nan
 
-    print (f'{j} / {stn_info.shape[0]}')
-
-fname = f'stn_code_data_{yr}.mat'
-matlab.savemat(path_stn_kor, fname, 'stn_code_data'), fname, {'ndata_scode':ndata_scode, 'header_ndata':header_ndata})
+    ndata_scode = None
+    # Assign scode2
+    for j in range(stn_info.shape[0]): 
+        ndata_temp = ndata[ndata[:,11]==stn_info[j,0],:]
+        for k in range(1,12+1):
+            ndata_temp2 = ndata_temp[ndata_temp[:,2]==k,:]
+            if len(ndata_temp2)!=0:
+                yrmon = yr*100+k
+                idx = (stn_info[j,4]<=yrmon)&(stn_info[j,5]>yrmon)
+                if idx==1:
+                    ndata_temp2[:,12]=stn_info[j,1]
+                    if ndata_scode is None:
+                        ndata_scode = ndata_temp2
+                    else:
+                        ndata_scode=np.vstack([ndata_scode,ndata_temp2])
+        print (f'{j+1} / {stn_info.shape[0]}')
+    fname = f'stn_scode_data_{yr}.mat'
+    matlab.savemat(os.path.join(path_stn_kr, 'stn_scode_data', fname), 
+                   {'ndata_scode':ndata_scode,'header_ndata':header_ndata})
